cb3a76625b6667acfb39a3410d5a2f34a171e116
Aarav Juneja <aarav@aaravjuneja.com>
2026-03-08 15:36:24 -0700

    Add intro to repo

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -12,6 +12,13 @@ def main():
     name = "Aarav Juneja"  # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("""Hi, I'm Aarav Juneja, a current sophomore at Amador Valley High School in California.
+
+I enjoy cybersecurity from CTFs to Cyber Defense competitions such as Cyberpatriot. I also enjoy competitive programming with USACO. I have explored a variety of different fields from working in assembly to simple websites and have some basic level of expertise in a wide range of topics.
+I currently use Ubuntu on my PC and Windows on my laptop, although I have dual + triple boots setup with Arch Linux and competition-specific Linux distros.
+
+I am excited to learn through this course and hope to further my skills in this interesting field.
+""")
 
 
 if __name__ == "__main__":
